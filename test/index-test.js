4984a11c515d35bddf52ff360c6cea306f9e8b63
Kindred Lee <kindred4lee@gmail.com>
2021-09-30 14:51:56 -0700

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,26 @@
+const { expect } = require("chai");
 const { name, height, message } = require("../index.js");
 
+describe("What is a test", () => {
+  describe("Name", () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual("Susan");
+    });
+  });
+
+  describe("Height", () => {
+    it("is less than 40", () => {
+      expect(height).toBeLessThan(40);
+    });
+  });
+
+  describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(message).toInclude(name);
+      expect(message).toInclude(height);
+    });
+  });
+});
 /*
 describe('what-is-a-test', () => {
   describe('Name', () => {
